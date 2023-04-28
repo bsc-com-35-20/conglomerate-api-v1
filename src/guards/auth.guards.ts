9fb7fc35d11e68cec664cbeef0f88a4ab9cffb74
bsc-com-35-20 <bsc-com-35-20@unima.ac.mw>
2023-04-28 12:22:08 +0100

    I have changed guards based on the guideline from NestJS documentation

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -1,33 +1,16 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
-
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const req = context.switchToHttp().getRequest();
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      return false;
-    }
-
-    const [bearer, token] = authHeader.split(' ');
-
-    if (bearer !== 'Bearer' || !token) {
-      return false;
-    }
-
-    try {
-      const decoded = this.jwtService.verify(token);
-      req.user = decoded;
-      return true;
-    } catch (err) {
-      return false;
-    }
+    const request = context.switchToHttp().getRequest();
+    return validateRequest(request);
   }
+}
+
+function validateRequest(request: any): boolean | Promise<boolean> | Observable<boolean> {
+  throw new Error('Function not implemented.');
 }
--- src/guards/roles.guards.ts
+++ src/guards/roles.guards.ts
@@ -1,26 +1,11 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) {
-      return true;
-    }
-
-    const req = context.switchToHttp().getRequest();
-    const user = req.user;
-
-    if (!user) {
-      return false;
-    }
-
-    return roles.includes(user.role);
+    return true;
   }
 }
