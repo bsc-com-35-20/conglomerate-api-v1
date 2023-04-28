c79b8aa41ac0b6c604b98e81836dd51cfe8527a1
bsc-com-35-20 <bsc-com-35-20@unima.ac.mw>
2023-04-28 12:55:04 +0100

    I have implemented the logging interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationExceptionFilter } from './errors/validation-exception.filter';
 import { HttpExceptionFilter } from './errors/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
   
