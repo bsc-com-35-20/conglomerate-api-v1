ef676972fb148fe1c838a62aea4975e955a1ef55
bsc-com-35-20 <bsc-com-35-20@unima.ac.mw>
2023-04-28 14:33:49 +0100

    i have implemented Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationExceptionFilter } from './errors/validation-exception.filter';
 import { HttpExceptionFilter } from './errors/http-exception.filter';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
+
+  const config = new DocumentBuilder()
+  .setTitle('Conglomerate web app')
+  .setDescription('Combination of student business')
+  .setVersion('1.0.4')
+  .addTag('')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(3000);
 }
